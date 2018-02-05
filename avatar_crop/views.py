c8090d13783f34c75fc28f7d745ac470331f9da8
Dan Collins <dcollinsn@gmail.com>
2018-02-05 17:20:45 -0500

    Django 2.0

--- avatar_crop/views.py
+++ avatar_crop/views.py
@@ -5,7 +5,7 @@ from django.core.files.base import ContentFile
 from django.shortcuts import render
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext as _
 from django.contrib.auth.decorators import login_required
